1440870ae440348f9f3659d99a7a53815e6128eb
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2019-01-23 12:32:40 +0100

    Add unfinished sound effect dictionary decoding

---
+++ src/main/kotlin/io/github/bartvhelvert/jagex/fs/osrs/dict/SoundEffectDictionary.kt
@@ -0,0 +1,26 @@
+package io.github.bartvhelvert.jagex.fs.osrs.dict
+
+import io.github.bartvhelvert.jagex.fs.Dictionary
+import io.github.bartvhelvert.jagex.fs.DictionaryCompanion
+import io.github.bartvhelvert.jagex.fs.JagexCache
+import io.github.bartvhelvert.jagex.fs.osrs.sound.SoundEffect
+
+class SoundEffectDictionary(
+    val soundEffects: List<SoundEffect>
+): Dictionary {
+
+    companion object : DictionaryCompanion<SoundEffectDictionary>() {
+        override val id = 4
+
+        @ExperimentalUnsignedTypes
+        override fun load(cache: JagexCache): SoundEffectDictionary {
+            val soundEffects = mutableListOf<SoundEffect>()
+            cache.readArchives(id).forEach { _, archive ->
+                archive.files.forEach { _, file ->
+                    soundEffects.add(SoundEffect.decode(file.data)) //TODO fix the decoding
+                }
+            }
+            return SoundEffectDictionary(soundEffects)
+        }
+    }
+}
