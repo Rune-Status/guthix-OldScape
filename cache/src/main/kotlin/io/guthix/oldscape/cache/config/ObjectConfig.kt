0f63b1e43c484e9d131dfc5ea228392a7580764e
Bart van Helvert <B.A.J.v.Helvert@student.tue.nl>
2019-11-29 00:39:33 +0100

    cache: make object config constructable

--- cache/src/main/kotlin/io/guthix/oldscape/cache/config/ObjectConfig.kt
+++ cache/src/main/kotlin/io/guthix/oldscape/cache/config/ObjectConfig.kt
@@ -22,51 +22,52 @@ import io.netty.buffer.ByteBuf
 import io.netty.buffer.Unpooled
 import java.io.IOException
 
-data class ObjectConfig(override val id: Int): Config(id) {
-    var name = "null"
-    var width: Short= 1
-    var length: Short = 1
-    var mapIconId: Int? = null
-    val options = arrayOfNulls<String>(5)
-    var clipType = 2
-    var isClipped = true
-    var modelClipped = false
-    var isHollow = false
-    var impenetrable = true
-    var accessBlock: Short = 0
-    var objectModels: IntArray? = null
-    var objectTypes: ShortArray? = null
-    var colorReplace: IntArray? = null
-    var colorFind: IntArray? = null
-    var textureFind: IntArray? = null
-    var textureReplace: IntArray? = null
-    var anInt2088: Short? = null
-    var animationId: Int? = null
-    var ambient: Byte = 0
-    var contrast = 0
-    var mapSceneId: Int? = null
-    var modelSizeX: Int = 128
-    var modelSizeHeight: Int = 128
-    var modelSizeY: Int = 128
-    var offsetX: Short = 0
-    var offsetHeight: Short = 0
-    var offsetY: Short = 0
-    var decorDisplacement: Short = 16
-    var isMirrored = false
-    var obstructsGround = false
-    var nonFlatShading = false
-    var contouredGround: Int? = null
-    var supportItems: Short? = null
-    var transformVarbit: Int? = null
-    var transformVarp: Int? = null
-    var transforms: Array<Int?>? = null
-    var ambientSoundId: Int? = null
-    var anInt2112: Int = 0
-    var anInt2113: Int = 0
-    var anInt2083: Short = 0
-    var anIntArray2084: IntArray? = null
-    var params: MutableMap<Int, Any>? = null
-
+data class ObjectConfig(
+    override val id: Int,
+    val name: String = "null",
+    val width: Short= 1,
+    val length: Short = 1,
+    val mapIconId: Int? = null,
+    val options: Array<String?> = arrayOfNulls<String>(5),
+    val clipType: Int = 2,
+    val isClipped: Boolean = true,
+    val modelClipped: Boolean = false,
+    val isHollow: Boolean = false,
+    val impenetrable: Boolean = true,
+    val accessBlock: Short = 0,
+    val objectModels: IntArray? = null,
+    val objectTypes: ShortArray? = null,
+    val colorReplace: IntArray? = null,
+    val colorFind: IntArray? = null,
+    val textureFind: IntArray? = null,
+    val textureReplace: IntArray? = null,
+    val anInt2088: Short? = null,
+    val animationId: Int? = null,
+    val ambient: Byte = 0,
+    val contrast: Int = 0,
+    val mapSceneId: Int? = null,
+    val modelSizeX: Int = 128,
+    val modelSizeHeight: Int = 128,
+    val modelSizeY: Int = 128,
+    val offsetX: Short = 0,
+    val offsetHeight: Short = 0,
+    val offsetY: Short = 0,
+    val decorDisplacement: Short = 16,
+    val isMirrored: Boolean = false,
+    val obstructsGround: Boolean = false,
+    val nonFlatShading: Boolean = false,
+    val contouredGround: Int? = null,
+    val supportItems: Short? = null,
+    val transformVarbit: Int? = null,
+    val transformVarp: Int? = null,
+    val transforms: Array<Int?>? = null,
+    val ambientSoundId: Int? = null,
+    val anInt2112: Int = 0,
+    val anInt2113: Int = 0,
+    val anInt2083: Short = 0,
+    val anIntArray2084: IntArray? = null,
+    val params: MutableMap<Int, Any>? = null
+) : Config(id) {
     override fun encode(): ByteBuf {
         val data = Unpooled.buffer()
         objectModels?.let {
@@ -118,7 +119,7 @@ data class ObjectConfig(override val id: Int): Config(id) {
         }
         if(nonFlatShading) data.writeOpcode(22)
         if(modelClipped) data.writeOpcode(23)
-        if(animationId == null) data.writeShort(65535) else data.writeShort(animationId!!.toInt())
+        if(animationId == null) data.writeShort(65535) else data.writeShort(animationId.toInt())
         if(clipType == 1) data.writeOpcode(27)
         if(decorDisplacement.toInt() != 16) {
             data.writeOpcode(28)
@@ -239,97 +240,135 @@ data class ObjectConfig(override val id: Int): Config(id) {
         override val id = 6
 
         override fun decode(id: Int, data: ByteBuf): ObjectConfig {
-            val objectConfig = ObjectConfig(id)
+            var name = "null"
+            var width: Short= 1
+            var length: Short = 1
+            var mapIconId: Int? = null
+            val options = arrayOfNulls<String>(5)
+            var clipType = 2
+            var isClipped = true
+            var modelClipped = false
+            var isHollow = false
+            var impenetrable = true
+            var accessBlock: Short = 0
+            var objectModels: IntArray? = null
+            var objectTypes: ShortArray? = null
+            var colorReplace: IntArray? = null
+            var colorFind: IntArray? = null
+            var textureFind: IntArray? = null
+            var textureReplace: IntArray? = null
+            var anInt2088: Short? = null
+            var animationId: Int? = null
+            var ambient: Byte = 0
+            var contrast = 0
+            var mapSceneId: Int? = null
+            var modelSizeX: Int = 128
+            var modelSizeHeight: Int = 128
+            var modelSizeY: Int = 128
+            var offsetX: Short = 0
+            var offsetHeight: Short = 0
+            var offsetY: Short = 0
+            var decorDisplacement: Short = 16
+            var isMirrored = false
+            var obstructsGround = false
+            var nonFlatShading = false
+            var contouredGround: Int? = null
+            var supportItems: Short? = null
+            var transformVarbit: Int? = null
+            var transformVarp: Int? = null
+            var transforms: Array<Int?>? = null
+            var ambientSoundId: Int? = null
+            var anInt2112: Int = 0
+            var anInt2113: Int = 0
+            var anInt2083: Short = 0
+            var anIntArray2084: IntArray? = null
+            var params: MutableMap<Int, Any>? = null
             decoder@ while (true) {
                 when (val opcode = data.readUnsignedByte().toInt()) {
                     0 -> break@decoder
                     1 -> {
                         val size = data.readUnsignedByte().toInt()
                         if (size > 0) {
-                            objectConfig.objectModels = IntArray(size)
-                            objectConfig.objectTypes = ShortArray(size)
+                            objectModels = IntArray(size)
+                            objectTypes = ShortArray(size)
                             for (i in 0 until size) {
-                                objectConfig.objectModels!![i] = data.readUnsignedShort()
-                                objectConfig.objectTypes!![i] = data.readUnsignedByte()
+                                objectModels[i] = data.readUnsignedShort()
+                                objectTypes[i] = data.readUnsignedByte()
                             }
                         }
                     }
-                    2 -> objectConfig.name = data.readStringCP1252()
+                    2 -> name = data.readStringCP1252()
                     5 -> {
                         val size = data.readUnsignedByte().toInt()
                         if (size > 0) {
-                            objectConfig.objectTypes = null
-                            objectConfig.objectModels = IntArray(size) { data.readUnsignedShort() }
+                            objectTypes = null
+                            objectModels = IntArray(size) { data.readUnsignedShort() }
                         }
                     }
-                    14 -> objectConfig.width = data.readUnsignedByte()
-                    15 -> objectConfig.length = data.readUnsignedByte()
+                    14 -> width = data.readUnsignedByte()
+                    15 -> length = data.readUnsignedByte()
                     17 -> {
-                        objectConfig.clipType = 0
-                        objectConfig.impenetrable = false
+                        clipType = 0
+                        impenetrable = false
                     }
-                    18 -> objectConfig.impenetrable = false
-                    19 -> objectConfig.anInt2088 = data.readUnsignedByte()
-                    21 -> objectConfig.contouredGround = 0
-                    22 -> objectConfig.nonFlatShading = true
-                    23 -> objectConfig.modelClipped = true
+                    18 -> impenetrable = false
+                    19 -> anInt2088 = data.readUnsignedByte()
+                    21 -> contouredGround = 0
+                    22 -> nonFlatShading = true
+                    23 -> modelClipped = true
                     24 -> {
-                        objectConfig.animationId = data.readUnsignedShort()
-                        if(objectConfig.animationId!!.toInt() == 65535) {
-                            objectConfig.animationId = null
+                        animationId = data.readUnsignedShort()
+                        if(animationId!!.toInt() == 65535) {
+                            animationId = null
                         }
                     }
-                    27 -> objectConfig.clipType = 1
-                    28 -> objectConfig.decorDisplacement = data.readUnsignedByte()
-                    29 -> objectConfig.ambient = data.readByte()
-                    39 -> objectConfig.contrast = data.readByte().toInt() * 25
-                    in 30..34 -> objectConfig.options[opcode - 30] = data.readStringCP1252().takeIf { it != "Hidden" }
+                    27 -> clipType = 1
+                    28 -> decorDisplacement = data.readUnsignedByte()
+                    29 -> ambient = data.readByte()
+                    39 -> contrast = data.readByte().toInt() * 25
+                    in 30..34 -> options[opcode - 30] = data.readStringCP1252().takeIf { it != "Hidden" }
                     40 -> {
-                        val colorsSize = data.readUnsignedByte().toInt()
-                        val colorFind = IntArray(colorsSize)
-                        val colorReplace = IntArray(colorsSize)
-                        for (i in 0 until colorsSize) {
+                        val amount = data.readUnsignedByte().toInt()
+                        colorFind = IntArray(amount)
+                        colorReplace = IntArray(amount)
+                        for (i in 0 until amount) {
                             colorFind[i] = data.readUnsignedShort()
                             colorReplace[i] = data.readUnsignedShort()
                         }
-                        objectConfig.colorFind = colorFind
-                        objectConfig.colorReplace = colorReplace
                     }
                     41 -> {
-                        val texturesSize = data.readUnsignedByte().toInt()
-                        val textureFind = IntArray(texturesSize)
-                        val textureReplace = IntArray(texturesSize)
-                        for (i in 0 until texturesSize) {
+                        val amount = data.readUnsignedByte().toInt()
+                        textureFind = IntArray(amount)
+                        textureReplace = IntArray(amount)
+                        for (i in 0 until amount) {
                             textureFind[i] = data.readUnsignedShort()
                             textureReplace[i] = data.readUnsignedShort()
                         }
-                        objectConfig.textureFind = textureFind
-                        objectConfig.textureReplace = textureReplace
                     }
-                    62 -> objectConfig.isMirrored = true
-                    64 -> objectConfig.isClipped = false
-                    65 -> objectConfig.modelSizeX = data.readUnsignedShort()
-                    66 -> objectConfig.modelSizeHeight = data.readUnsignedShort()
-                    67 -> objectConfig.modelSizeY = data.readUnsignedShort()
-                    68 -> objectConfig.mapSceneId = data.readUnsignedShort()
-                    69 -> objectConfig.accessBlock = data.readUnsignedByte()
-                    70 -> objectConfig.offsetX = data.readShort()
-                    71 -> objectConfig.offsetHeight = data.readShort()
-                    72 -> objectConfig.offsetY = data.readShort()
-                    73 -> objectConfig.obstructsGround = true
-                    74 -> objectConfig.isHollow = true
-                    75 -> objectConfig.supportItems = data.readUnsignedByte()
+                    62 -> isMirrored = true
+                    64 -> isClipped = false
+                    65 -> modelSizeX = data.readUnsignedShort()
+                    66 -> modelSizeHeight = data.readUnsignedShort()
+                    67 -> modelSizeY = data.readUnsignedShort()
+                    68 -> mapSceneId = data.readUnsignedShort()
+                    69 -> accessBlock = data.readUnsignedByte()
+                    70 -> offsetX = data.readShort()
+                    71 -> offsetHeight = data.readShort()
+                    72 -> offsetY = data.readShort()
+                    73 -> obstructsGround = true
+                    74 -> isHollow = true
+                    75 -> supportItems = data.readUnsignedByte()
                     77, 92 -> {
-                        val transformVarbit = data.readUnsignedShort()
-                        objectConfig.transformVarbit = if(transformVarbit == 65535) null else transformVarbit
-                        val transformVarp = data.readUnsignedShort()
-                        objectConfig.transformVarp = if(transformVarbit == 65535) null else transformVarp
+                        transformVarbit = data.readUnsignedShort()
+                        transformVarbit = if(transformVarbit == 65535) null else transformVarbit
+                        transformVarp = data.readUnsignedShort()
+                        transformVarp = if(transformVarbit == 65535) null else transformVarp
                         val lastEntry = if(opcode == 92) {
                             val entry = data.readUnsignedShort()
                             if(entry == 65535) null else entry
                         } else null
                         val size = data.readUnsignedByte().toInt()
-                        val transforms = arrayOfNulls<Int?>(size + 2)
+                        transforms = arrayOfNulls<Int?>(size + 2)
                         for(i in 0..size) {
                             val transform = data.readUnsignedShort()
                             transforms[i] = if(transform == 65535) null else transform
@@ -337,46 +376,51 @@ data class ObjectConfig(override val id: Int): Config(id) {
                         if(opcode == 92) {
                             transforms[size + 1] = lastEntry
                         }
-                        objectConfig.transforms = transforms
                     }
                     78 -> {
-                        objectConfig.ambientSoundId = data.readUnsignedShort()
-                        objectConfig.anInt2083 = data.readUnsignedByte()
+                        ambientSoundId = data.readUnsignedShort()
+                        anInt2083 = data.readUnsignedByte()
                     }
                     79 -> {
-                        objectConfig.anInt2112 = data.readUnsignedShort()
-                        objectConfig.anInt2113 = data.readUnsignedShort()
-                        objectConfig.anInt2083 = data.readUnsignedByte()
+                        anInt2112 = data.readUnsignedShort()
+                        anInt2113 = data.readUnsignedShort()
+                        anInt2083 = data.readUnsignedByte()
                         val size = data.readUnsignedByte().toInt()
-                        objectConfig.anIntArray2084 = IntArray(size) { data.readUnsignedShort() }
+                        anIntArray2084 = IntArray(size) { data.readUnsignedShort() }
                     }
-                    81 -> objectConfig.contouredGround = data.readUnsignedByte() * 256
-                    82 -> objectConfig.mapIconId = data.readUnsignedShort()
-                    249 -> objectConfig.params = data.readParams()
+                    81 -> contouredGround = data.readUnsignedByte() * 256
+                    82 -> mapIconId = data.readUnsignedShort()
+                    249 -> params = data.readParams()
                     else -> throw IOException("Did not recognise opcode $opcode.")
                 }
             }
-            if (objectConfig.anInt2088 == null) {
-                objectConfig.anInt2088 = 0
-                if ((objectConfig.objectModels != null && (objectConfig.objectTypes == null)
-                            || objectConfig.objectTypes?.get(0)?.toInt() == 10)
+            if (anInt2088 == null) {
+                anInt2088 = 0
+                if ((objectModels != null && (objectTypes == null)
+                            || objectTypes?.get(0)?.toInt() == 10)
                 ) {
-                    objectConfig.anInt2088 = 1
+                    anInt2088 = 1
                 }
                 for (i in 0 until 5) {
-                    if (objectConfig.options[i] != null) {
-                        objectConfig.anInt2088 = 1
+                    if (options[i] != null) {
+                        anInt2088 = 1
                     }
                 }
             }
-            if (objectConfig.supportItems == null) {
-                objectConfig.supportItems = if(objectConfig.clipType != 0) 1 else 0
+            if (supportItems == null) {
+                supportItems = if(clipType != 0) 1 else 0
             }
-            if (objectConfig.isHollow) {
-                objectConfig.clipType = 0
-                objectConfig.impenetrable = false
+            if (isHollow) {
+                clipType = 0
+                impenetrable = false
             }
-            return objectConfig
+            return ObjectConfig(id, name, width, length, mapIconId, options, clipType, isClipped, modelClipped,
+                isHollow, impenetrable, accessBlock, objectModels, objectTypes, colorReplace, colorFind, textureFind,
+                textureReplace, anInt2088, animationId, ambient, contrast, mapSceneId, modelSizeX, modelSizeHeight,
+                modelSizeY, offsetX, offsetHeight, offsetY, decorDisplacement, isMirrored, obstructsGround,
+                nonFlatShading, contouredGround, supportItems, transformVarbit, transformVarp, transforms,
+                ambientSoundId, anInt2112, anInt2113, anInt2083, anIntArray2084, params
+            )
         }
     }
 }
